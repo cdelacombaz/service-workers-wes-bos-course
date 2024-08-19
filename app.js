6e6b50e57d901e24c79c478b427ca153f2a9fd99
cdelacombaz <cdelacombaz@bluewin.ch>
2024-08-19 11:34:13 +0200

    fix: deployment

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ async function start() {
 start();
 
 // this is just a little bit of code that makes our tooling reload the page if and then the modules are updated. For development only
-if (module.hot) {
-    module.hot.dispose(() => {
-        window.location.reload();
-    });
-}
+// if (module.hot) {
+//     module.hot.dispose(() => {
+//         window.location.reload();
+//     });
+// }
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
